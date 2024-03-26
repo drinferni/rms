551022755e1f7fa9e8f4d858736fef1d575eb598
Ansh Meshram <anshmeshram04@yah00.com>
2024-03-26 21:05:09 +0530

    26/03/2024

--- Client/src/components/Vision.vue
+++ Client/src/components/Vision.vue
@@ -3,21 +3,37 @@
     <button @click="gotohome" > Home </button>
 
     <button @click="uploadData">Upload</button>
+    <button @click="getData">GETDATA</button>
 </template>
 
 <script>
 import axios from 'axios';
+//import { response } from 'express';
 
 export default {
     name:"visionComp",
+    data () {
+        return {
+            i:10,
+        }
+    },
     methods : {
         gotohome () {
             this.$emit("home")
         },
 
-        async uploadData () {
-            let result = await axios.post("http://localhost:3001/data", {image_id: 1001, image:"231"} )
-            console.log(result);
+        // async uploadData () {
+        //     let result = await axios.post('localhost:3000');
+        //     console.log(result);
+        // }
+
+        async getData() {
+            try {
+                const response = await axios.get('http://localhost:3000/data/'+ this.i);
+                console.log(response.data); // Assuming you want to log the response data
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     }
 }
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+
+const inputrouter = require("./Routers/InputRouters");
+
+
 const app = express();
 app.use(cors());
-const PORT = 3001;
+const PORT = 3000;
 
+app.use(express.json());
 function connnectionstatus() {
     console.log("connected");
 }
 
 app.listen(PORT,connnectionstatus)
 
+app.use ( inputrouter);
 
 // app.post("/data",(request,response)=>{
 //     response.send("images")
 // })
 
-// app.get("/data",(request,response)=> {
-//     response.send("images")
-// })
+app.get("/data",(request,response)=> {
+    response.send({"images":10})
+})
+
+app.get("/data/10",(request,response)=> {
+    response.send({"images":100})
+})
+
+app.post("/data",(request,response)=> {
+    console.log(request.body);
+    response.send(201);
+})
+console.log("all routers are ready")
