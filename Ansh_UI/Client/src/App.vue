9f966ccb4860b399c27fd40a1e9e7af3f539f45c
Ansh Meshram <anshmeshram04@yah00.com>
2024-04-13 19:42:08 +0530

    13-04-2024

--- Ansh_UI/Client/src/App.vue
+++ Ansh_UI/Client/src/App.vue
@@ -2,16 +2,17 @@
   <div v-if="choose" id="master-container">
     <div id="main-container">
       <div id="vision-wrapper">
-        <div  id="Vision">
-            <div id="animation">
-             <svg id="vision-anime"  width="484" height="202" viewBox="0 0 484 402" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M251 0.5H234.5L0 402H484L251 0.5Z" fill="#E7EC0B"/>
-            </svg>
-
-
+        <div  id="Vision" @mouseenter="hover=true" @mouseleave="hover =false"  @click="choosevision">
+          <div id="camera" >
+            <img  src="./assets/camera-image.webp" height="75px" width="75px">
           </div>
-          <div class="button-wrapper">
-            <button class="button" @click="choosevision">VISION</button>
+          <div id="vision-anime">
+            <svg   width="490" height="260" viewBox="0 0 490 260" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path :d="imgsize" fill="#E7EC0B"/>
+             </svg>
+          </div>
+          <div id="track-vision">
+            <img  src="./assets/2.jpg" height="100px" width="500px">
           </div>
         </div>
       </div>
@@ -27,8 +28,6 @@
           </div>
         </div>
       </div>
-
-
     </div>
   </div>
  <visionComp @home="choosechoose" v-if="enableVision"></visionComp>
@@ -50,11 +49,19 @@ export default {
     return {
       choose:1,
       enableUltrasound:0,
-      enableVision:0
+      enableVision:0,
+      hover : false,
+      imgsize :"M 250 0  H 240  L 0 260  H 490 L 250 0  Z",
+      imgsize1 : "M 250 0  H 240  L ",
+      height : 0,
+      diff : 0,
+      imgsize2 : "L 250 0  Z"
 
     }
   },
-  methods : {
+  methods: {
+
+    
 
     choosevision () {
       this.choose=0
@@ -71,7 +78,6 @@ export default {
         this.enableVision=0
         this.enableUltrasound=0
     },
-
   },
 }
 
@@ -125,18 +131,32 @@ need to add a new text font . add colour and make it good till end of week
   height : 400px;
   width : 500px;
   display : grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
+  grid-template-columns: repeat(20,1fr);
+  grid-template-rows: repeat(20,1fr);
   align-items : center;
   justify-items : center;
   border : 5px;
   border-style: solid;
+}
 
+#animation-vision {
+  display:grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 5fr 1fr;
+}
 
+#camera {
+  grid-area: 2 / 9 / 4 / 13;
 }
 
 #vision-anime {
-background: url("data:image/svg+xml,%3Csvg width='484' height='202' viewBox='0 0 484 402' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M251 0.5H234.5L0 402H484L251 0.5Z' fill='%23E7EC0B'/%3E%3C/svg%3E%0A");
+  grid-area: 4 / 2 / 17 / 20;
 }
+
+#track-vision {
+  grid-area: 16 / 2 / 20 / 20;
+}
+
+
 
 </style>
